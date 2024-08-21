b2b89e7a8376d65ccd0b34088dfc991f0a558032
OFF <peerapat13th@gmail.com>
2024-08-21 13:57:51 +0700

    Chnage page and scrolling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+// Import AppComponent from app.component.ts
+import { AppComponent } from './app/app.component';
+//Start page from above
+import {
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling,
+  withViewTransitions
+} from '@angular/router';
+
+import { routes } from './app/app.routes';
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes, 
+      withInMemoryScrolling(scrollConfig),
+      withViewTransitions()
+    )
+  ]
+})
   .catch((err) => console.error(err));
